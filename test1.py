93d42d297c345b46e9b713176d4e5eba40c4b6c0
cxcxzxcx <45855096+cxcxzxcx@users.noreply.github.com>
2018-12-17 19:36:13 +0200

    17/12/18

--- test1.py
+++ test1.py
@@ -1,4 +1,14 @@
+import random
+
+
 print("hi")
 print("hi")
 print("hi")
 print("blat")
+
+i = 0
+
+while i < 10 :
+    s = random.choice('Chewbacca') + str(random.randint(1, 100))
+    print (s)
+    i = i + 1
